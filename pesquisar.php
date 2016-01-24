93c0432874b695316e17986467604d0057626345
lbsonweb <lbs.on.web@gmail.com>
2016-01-24 14:44:26 -0200

    Create pesquisar.php

---
+++ pesquisar.php
@@ -0,0 +1,17 @@
+<?php
+require 'conecta.inc';
+$pesquisa=$_POST['pesquisa'];
+
+$sql="SELECT * FROM itens WHERE produto LIKE'%$pesquisa%'";
+$res=mysqli_query($con, $sql);
+
+While($reg=mysqli_fetch_row($res)){
+	$vproduto=$reg[1];
+	$vpreco=$reg[2];
+	$vquant=$reg[3];
+	echo "<table border= 1 >
+			<tr>
+				<td>$vproduto</td><td> $vpreco</td><td> $vquant</td>
+		  </tr
+	</table>";
+}
